a3191b0e22d09c1d7131fc4e37d2285ce18e4c9f
Paul Beard <pbeardey@gmail.com>
2022-05-02 18:27:23 +0100

    read artist, write test and pass

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -7,7 +7,7 @@ exports.createArtist = async (req, res) => {
 
     try {
         await db.query(
-            "INSERT INTO Artist (name, genre) VALUES (?,?)",
+            'INSERT INTO Artist (name, genre) VALUES (?,?)',
             [
                 name,
                 genre
@@ -22,4 +22,16 @@ exports.createArtist = async (req, res) => {
     await db.end();
  };
 
+ exports.readArtist = async (req, res) => {
+    const db = await getDb();
+
+    try {
+        const [artists] = await db.query('SELECT * FROM Artist');
+        res.status(200).json(artists);
+    } catch(err) {
+        res.sendStatus(500).json(err);
+    }
+
+    await db.end();
+ }
 
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const artistController = require('../controllers/artist');
 
+router.get ('/', artistController.readArtist);
 
 router.post ('/', artistController.createArtist);
+
+
 
 
 module.exports = router;
